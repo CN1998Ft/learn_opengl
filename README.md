4d0684d41dc2f912ce0dfa5af63ff1d057867d58
Fengtao Zhang <zhangfengtao1998@gmail.com>
2026-07-13 00:05:44 +0100

    setup on mac

---
+++ README.md
@@ -0,0 +1,12 @@
+# Learn OpenGL Notes and Codes
+
+<!-- TODO: How to initialise the libraries and stuff. -->
+> [!NOTE]
+> This repository is created to follow the learn OpenGL. And the aim is to have
+> this repo working with both Windows, MacOs, and Linux.
+
+Therefore, the version of OpenGL is tied to version 4.1 as the latest version
+available across all platform (it is mainly Apple which deprecated OpenGL after
+version 4.1).
+Therefore, using ***GLAD*** as the extension loader generator and ***glfw***
+for the window creation.
---
+++ code/build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")" && echo "[Current dir: $PWD]"
+
+build()
+{
+    mac='^darwin.*$'
+    pushd ../build > /dev/null 2>&1
+
+    INCLUDE="-I../code/include"
+    CFLAGS="-std=c++17 -Wall -g"
+    SRC="../code/gl.c ../code/*.cpp"
+    LIBS="-lglfw"
+    if [[ "$OSTYPE" =~ $mac ]]; then
+        echo "[Building learning OpenGL project]"
+        INCLUDE="$INCLUDE -I/opt/homebrew/include"
+        LIBS="$LIBS -L/opt/homebrew/lib \
+-framework OpenGL -framework Cocoa -framework IOKit \
+-framework CoreVideo"
+    fi
+
+    echo "gcc $CFLAGS $INCLUDE $SRC $LIBS -o main"
+    g++ $CFLAGS $INCLUDE $SRC $LIBS -o main
+
+    popd > /dev/null 2>&1
+}
+
+if [[ ! -d ../build ]]; then
+    mkdir ../build > /dev/null 2>&1
+    echo '*' > ../build/.gitignore
+fi
+
+if [[ "$#" == 0 ]]; then
+    build
+elif [[ "$1" == "clean" ]]; then
+    rm -rf ../build > /dev/null 2>&1
+fi
